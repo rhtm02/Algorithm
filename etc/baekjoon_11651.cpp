e8bfcbdd0f067377b8100a73736d523da1e6ec5e
이형준 <rhtm02@gmail.com>
2021-06-02 13:31:29 +0900

    Solve 20210602
    Lessons : pair 컨테이너 사용법
    sort의 compare 인자 사용법

---
+++ etc/baekjoon_11651.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second) return a.first < b.first;
+	return a.second < b.second;
+}
+
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	vector<pair<int, int>> values;
+
+	for (int i = 0; i < N; i++)
+	{
+		pair<int, int> temp;
+		scanf("%d %d", &temp.first, &temp.second);
+		values.push_back(temp);
+	}
+
+	sort(values.begin(), values.end(), compare);
+	
+	for (auto it : values) printf("%d %d \n", it.first, it.second);
+
+}
