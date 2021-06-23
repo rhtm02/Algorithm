99ef3602e70716b927e48a8ac47b561177cce545
Hyungjun <rhtm02@gmail.com>
2021-06-23 20:59:33 +0900

    Resolve 20210623

--- DP/baekjoon_1463.cpp
+++ DP/baekjoon_1463.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-int dp[1000001];
-
-int solution(int n)
-{
-    for (int i = 2; i <= n; i++)
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> DP(N + 1);
+    DP[1] = 0;
+    DP[2] = 1;
+    DP[3] = 1;
+    for(int i = 4; i <= N;i++)
     {
-        dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0)
-            dp[i] = min(dp[i / 2] + 1, dp[i]);
-        if (i % 3 == 0)
-            dp[i] = min(dp[i / 3] + 1, dp[i]);
+        if(((i%3) == 0) && ((i%2) != 0))
+        {
+            DP[i] = min(DP[i - 1] + 1,DP[i/3] + 1);
+        }
+        else if(((i%2) == 0) && ((i%3) != 0))
+        {
+            DP[i] = min(DP[i - 1] + 1,DP[i/2] + 1);
+        }
+        else if(((i%2) == 0) && ((i%3) == 0))
+        {
+            int temp = min(DP[i - 1] + 1,DP[i/2] + 1);
+            temp = min(temp,DP[i/3] + 1);
+            DP[i] = temp;
+        }
+        else
+        {
+            DP[i] = DP[i - 1] + 1;
+        }
     }
-
-    return dp[n];
-
-}
-int main()
-{
-    dp[1] = 0;
-    int n;
-    cin >> n;
-
-    cout << solution(n);
+    cout << DP[N];
 }
