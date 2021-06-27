be7f19b01785301da07339e7567352cac28b0797
Hyungjun <rhtm02@gmail.com>
2021-06-27 21:37:24 +0900

    Resolve 20210627

--- DP/baekjoon_11053.cpp
+++ DP/baekjoon_11053.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
-int arr[1001];
-vector<int> dp(1001, 1);
-
-int main()
-{
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (arr[i] > arr[j])
-                dp[i] = max(dp[j] + 1, dp[i]);
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> values(N + 1);
+    vector<int> DP(N + 1);
+    for(int i = 1;i <= N;i++) cin >> values[i];
+    DP[1] = 1;
+    int ans = 1;
+    for(int idx = 2;idx <= N;idx++)
+    {
+        int val = 0;
+        for(int temp = idx - 1;temp > 0;temp--)
+        {
+            if(values[temp] < values[idx]) val = max(val,DP[temp]);
         }
+        DP[idx] = val + 1;
+        ans = max(DP[idx],ans);
     }
-
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-        ans = max(ans, dp[i]);
-        
-    cout << ans << endl;
-    return 0;
+    cout << ans;
 }
