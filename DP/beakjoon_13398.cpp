f79e4b9a7237739e02c575c5d0fe50ec41f31a8f
Hyungjun <rhtm02@gmail.com>
2022-05-17 21:50:31 +0900

    Fail 20220517 DP의 핵심은 부분으로 나눠서 해결하는 것 한개 뺄수있을때/아닐때 나누는 아이디어 생각 못함

---
+++ DP/beakjoon_13398.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a[100001];
+int d[100001][2];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int ans = a[0];
+    for (int i = 0; i < n; i++) {
+        d[i][0] = d[i][1] = a[i];
+        if (i == 0) continue;
+        d[i][0] = max(d[i - 1][0] + a[i], a[i]);
+        d[i][1] = max(d[i - 1][0], d[i - 1][1] + a[i]);
+        ans = max(ans,max(d[i][0], d[i][1]));
+    }
+    cout << ans << '\n';
+    return 0;
+}
