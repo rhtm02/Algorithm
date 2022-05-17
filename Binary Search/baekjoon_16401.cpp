f8d8e605bc0a783c6cd3831f9908d2191e85ccd3
Hyungjun <rhtm02@gmail.com>
2022-05-17 20:29:34 +0900

    Solve 20220516 이진탐색 혼동 금지

--- Binary Search/baekjoon_16401.cpp
+++ Binary Search/baekjoon_16401.cpp
@@ -1,43 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <map>
 
+typedef unsigned long long ull;
 using namespace std;
 
+int N,M;
+vector<ull> inputs;
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
-    //입력
-    int M, N;
-    vector<int> L;
-    cin >> M >> N;
-    for (int i = 0; i < N; i++) {
-        int l;
-        cin >> l;
-        L.push_back(l);
+    cin >> M; cin >> N;
+
+    for(int i = 0;i < N;i++){
+        int temp;
+        cin >> temp;
+        inputs.push_back(temp);
     }
+    sort(inputs.begin(),inputs.end());
+    ull left = 1;
+    ull right = inputs[N - 1];
 
-    //이분탐색, 이진탐색
-    int left = 1, right = *max_element(L.begin(), L.end());
-    //left: 가장 작은 수, right: 가장 큰 수
-    int snack = 0;
-    while (left <= right) {
-        int mid = (left + right) / 2; //중앙값
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            count += L[i] / mid; //mid 과자 길이로 나눠줄수 있는 양
+    ull val;
+    ull ans = 0;
+    while(left <= right) {
+        val = (left + right) / 2;
+        ull nums = 0;
+        for(auto it:inputs){
+            nums += (it/val);
         }
-        if (count >= M) { //과자 나눠줄수 있을 때
-            left = mid + 1;
-            snack = mid;
+        if(nums >= M){
+            left = val + 1;
+            ans = val;
         }
-        else if (count < M) { //과자 나눠줄수 없을 때
-            right = mid - 1;
+        else{
+            right = val - 1;
         }
     }
-    cout << sum << "\n";
-
+    cout << ans;
     return 0;
 }
