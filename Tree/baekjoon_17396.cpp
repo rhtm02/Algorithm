4c5b680efd666100b5e16e0626422b87b471373f
Hyungjun <rhtm02@gmail.com>
2022-05-21 00:57:36 +0900

    Solve 20220521 일반적인 다익스트라 알고리즘에서 중복을 없애는 방법이 핵심 다익스트라는 DP의 응용임을 명심할 것

--- Tree/baekjoon_17396.cpp
+++ Tree/baekjoon_17396.cpp
@@ -1,88 +1,53 @@
 #include<iostream>
-#include<stdio.h>
 #include<algorithm>
 #include<vector>
 #include<map>
 #include<queue>
+#include<climits>
 using namespace std;
-typedef pair<int,pair<int, int>> PAIR;
+typedef pair<int, long long> PAIR;
 
 int N, M;
+
 vector<int> nodes;
+vector<long long> cost;
 
-vector<int> parents;
-
-vector<PAIR> graph;
-
-map<int,vector<pair<int,int>>> tree;
-
-vector<int> nodes_path;
+map<int,vector<PAIR>> graph;
 
 
-int ans = -1;
 
-bool compare(PAIR a, PAIR b) {
-	return a.second.second < b.second.second;
-}
+long long ans = -1;
 
-
-int find(int node) {
-	if (parents[node] == node) return node;
-
-	return find(parents[node]);
-}
-
-
-void union_set(int a, int b) {
-	int node1 = find(a);
-	int node2 = find(b);
-	if (node1 > node2) parents[node2] = node1;
-	else parents[node1] = node2;
-}
-
-
-void kruskal() {
-
-	sort(graph.begin(), graph.end(),compare);
-
-	for (auto it : graph) {
-		if ((nodes[it.first] == 1) || (nodes[it.second.first] == 1)) continue;
-
-		if (find(it.first) != find(it.second.first)) {
-			union_set(it.first, it.second.first);
-			tree[it.first].push_back({ it.second.first,it.second.second });
-			tree[it.second.first].push_back({ it.first,it.second.second });
-		}
-	}
-
-	for (auto it : tree) {
-		cout << it.first << " : ";
-		for (auto it2 : it.second) cout << it2.first << ' ' << it2.second;
-		cout << '\n';
-	}
-
-}
 
 struct cmp {
-	bool operator()(pair<int,int> a,pair<int,int> b) {
-		return a.second < b.second;
-	}
+    bool operator()(PAIR a,PAIR b) {
+        return a.second > b.second;
+    }
 };
 
 
 void path_find() {
-	priority_queue < pair<int, int>, vector<pair<int, int>>, cmp> pq;
+    priority_queue < PAIR, vector<PAIR>, cmp> pq;
 
-	pq.push({ 0,0 });
-	
-	while (!pq.empty()) {
-		pair<int, int> top = pq.top();
-		int node = top.first;
-		int weight = top.second;
+    pq.push({ 0,0 });
+    cost[0] = 0;
+    while (!pq.empty()) {
+        PAIR top = pq.top();
+        pq.pop();
+        int node = top.first;
+        long long weight = top.second;
+        if(cost[node] < weight) continue;
+        nodes[node] = 1;
 
-	}
+        for(auto it:graph[node]){
+            if((nodes[it.first] == 0) && (cost[it.first] > cost[node] + it.second))
+            {
+                cost[it.first] = cost[node] + it.second;
+                pq.push({it.first,cost[it.first]});
+            }
+        }
 
-	
+    }
 }
 
 
@@ -90,27 +55,31 @@ void path_find() {
 
 int main()
 {
-	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
-	cin >> N >> M;
+    cin >> N >> M;
 
-	for (int i = 0; i < N; i++) {
-		int c;
-		cin >> c;
-		nodes.push_back(c);
-		nodes_path.push_back(c);
-		parents.push_back(i);
-	}
+    for (int i = 0; i < N; i++) {
+        int c;
+        cin >> c;
+        if(i == N -1) {
+            nodes.push_back(0);
+        }
+        else nodes.push_back(c);
 
-	for (int i = 0; i < M; i++) {
-		int f, t,w;
-		cin >> f >> t >> w;
-		graph.push_back({f, { t,w } });
-		graph.push_back({t, { f,w } });
-	}
+        cost.push_back(LONG_LONG_MAX);
+    }
 
-	kruskal();
+    for (int i = 0; i < M; i++) {
+        int f, t;
+        long long w;
+        cin >> f >> t >> w;
+        graph[f].push_back({ t,w });
+        graph[t].push_back({ f,w });
+    }
 
+    path_find();
 
-
+    if(cost[N - 1] == LONG_LONG_MAX) cout << -1;
+    else cout << cost[N - 1];
 }
